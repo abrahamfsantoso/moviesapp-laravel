f2ef0fa6e811ef7e9cf11ca4a33f21bbfddc4504
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-10-27 11:02:39 +0700

    added search dropdown w/ livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,11 +7,12 @@
     <title>Movie App</title>
 
     <link rel="stylesheet" href="/css/main.css">
+    <livewire:styles>
 </head>
 
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-gray-800">
-        <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
+        <div class="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-6">
             <ul class="flex flex-col md:flex-row items-center">
                 <li>
                     <a href="{{ route('movies.index') }}">
@@ -33,26 +34,17 @@
                 </li>
             </ul>
             <div class="flex flex-col md:flex-row items-center">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text"
-                        class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
-                        placeholder="Search">
-                    <div class="absolute top-0">
-                        <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
-                            <path class="heroicon-ui"
-                                d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" />
-                        </svg>
+                <livewire:search-dropdown>
+                    <div class="md:ml-4 mt-3 md:mt-0">
+                        <a href="#">
+                            <img src="/img/nickfury.jpg" alt="avatar" class="rounded-full w-8 h-8">
+                        </a>
                     </div>
-                </div>
-                <div class="md:ml-4 mt-3 md:mt-0">
-                    <a href="#">
-                        <img src="/img/nickfury.jpg" alt="avatar" class="rounded-full w-8 h-8">
-                    </a>
-                </div>
             </div>
         </div>
     </nav>
     @yield('content')
+    <livewire:scripts>
 </body>
 
 </html>
